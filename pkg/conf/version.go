e37d112036a10b8d732c054227ca94bc5e46e92d
Ricardo Urrea <ricardo.urrea@gmail.com>
2024-04-09 07:47:15 +0200

    action: ldflags vars

--- pkg/conf/version.go
+++ pkg/conf/version.go
@@ -16,22 +16,22 @@ const banner = `
   ╚══════╝╚══════╝╚═╝╚═════╝ ╚══════╝╚═╝  ╚═╝`
 
 var (
-	version = "development"
+	Version = "development"
 	binOS   = runtime.GOOS
 	binArch = runtime.GOARCH
-	commit  = "undefined"
-	date    = "undefined"
-	goVer   = "undefined"
+	Commit  = "undefined"
+	Date    = "undefined"
+	GoVer   = "undefined"
 )
 
 func PrintVersion() {
 	fmt.Printf("%s\n\n", banner)
 	twl := new(tabwriter.Writer)
 	twl.Init(os.Stdout, 0, 4, 2, ' ', 0)
-	_, _ = fmt.Fprintf(twl, "\tVersion:\t%s\t\n", version)
+	_, _ = fmt.Fprintf(twl, "\tVersion:\t%s\t\n", Version)
 	_, _ = fmt.Fprintf(twl, "\tSystem:\t%s/%s\t\n", binOS, binArch)
-	_, _ = fmt.Fprintf(twl, "\tGit Commit:\t%s\t\n", commit)
-	_, _ = fmt.Fprintf(twl, "\tRelease Date:\t%s\t\n", date)
-	_, _ = fmt.Fprintf(twl, "\tGo Version:\t%s\t\n\n\n", goVer)
+	_, _ = fmt.Fprintf(twl, "\tGit Commit:\t%s\t\n", Commit)
+	_, _ = fmt.Fprintf(twl, "\tRelease Date:\t%s\t\n", Date)
+	_, _ = fmt.Fprintf(twl, "\tGo Version:\t%s\t\n\n\n", GoVer)
 	_ = twl.Flush()
 }
