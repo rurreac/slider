b721816bf381bf43a942a0877b9ee7759f4e6591
Ricardo Urrea <ricardo.urrea@gmail.com>
2025-12-21 20:14:53 +0100

    chore: merge interface into instance

--- pkg/instance/instance.go
+++ pkg/instance/instance.go
@@ -33,6 +33,13 @@ const (
 	ExecEndpoint       = "exec-endpoint"
 )
 
+// ChannelOpener defines the interface required to open channels and send requests
+// This allows abstracting specific SSH connections (like direct ssh.Conn or our custom RemoteConnection)
+type ChannelOpener interface {
+	OpenChannel(name string, payload []byte) (ssh.Channel, <-chan *ssh.Request, error)
+	SendRequest(name string, wantReply bool, payload []byte) (bool, []byte, error)
+}
+
 type Config struct {
 	Logger               *slog.Logger
 	SessionID            int64
